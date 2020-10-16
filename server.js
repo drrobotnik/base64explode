c9b5c31d128221c2b7460fb9183d3aad3082065f
Brandon Lavigne <blavigne@rehabs.com>
2020-10-15 21:21:43 -0700

    ser

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.static('build'))
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
